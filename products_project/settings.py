43407cb6e8d758461c41098b9c2e35528a11e2c6
Nicholas Wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-04-13 16:10:32 -0400

    Changed database settings + Created products app

--- products_project/settings.py
+++ products_project/settings.py
@@ -69,15 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -120,3 +111,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from products_project.local_settings import *
+except ImportError:
+    pass    
