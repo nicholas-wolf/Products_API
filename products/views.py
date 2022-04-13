adb62c6ee0ff8baba2fb446f5cf17a0e933f5fde
Nicholas Wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-04-13 19:20:46 -0400

    Added postman collection + added superuser + created paths to URLs + created functions product_table and product_detail to the views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from . serializers import ProductSerializer
+from . models import Product
 
-# Create your views here.
+@api_view(['GET','POST'])
+def products_table(request):
+    
+    
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)   
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
